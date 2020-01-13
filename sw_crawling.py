6dd69e84e661f675a1fd04694fbae63665abbf34
TaeJuneJoung <jtj0525@gmail.com>
2020-01-13 23:10:04 +0900

    Deploy crawler

--- sw_crawling.py
+++ sw_crawling.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.firefox.options import Options
 from bs4 import BeautifulSoup as bs
-import time, json
+import time, json, os
 
 options = Options()
 options.headless = True
